14b0864a05919eff4ce242dd2042dce21cd08efd
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2022-01-12 09:37:02 +0700

    tất cả quốc gia chạy postman ok

---
+++ ThiThucHanh/src/main/java/com/example/thithuchanh/controller/CountryController.java
@@ -0,0 +1,29 @@
+package com.example.thithuchanh.controller;
+
+import com.example.thithuchanh.model.Country;
+import com.example.thithuchanh.service.ICountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/countries")
+public class CountryController {
+    @Autowired
+    private ICountryService countryService;
+
+    @GetMapping("")
+    public ResponseEntity<Iterable<Country>> findAll() {
+        List<Country> countries = (List<Country>) countryService.findAll();
+        if (countries.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } return new ResponseEntity<>(countries, HttpStatus.OK);
+    }
+}
